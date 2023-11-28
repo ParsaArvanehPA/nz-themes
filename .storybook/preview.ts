de95e0536fbc2d6de1bcebdb6bea45a68ca75dbe
mostmand <mostmand.mh@gmail.com>
2023-11-28 10:37:06 +0330

    Some fixes to welcome.component.stories.ts styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from "@storybook/angular";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
+import {importProvidersFrom} from "@angular/core";
+import {applicationConfig} from "@storybook/angular";
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +14,11 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    applicationConfig({
+      providers: [importProvidersFrom(BrowserModule), provideAnimations()],
+    })
+  ],
 };
 
 export default preview;
--- src/app/pages/welcome/welcome.component.stories.ts
+++ src/app/pages/welcome/welcome.component.stories.ts
@@ -1,10 +1,18 @@
 import type {Meta, StoryObj} from '@storybook/angular';
 import {WelcomeComponent} from "./welcome.component";
+import {applicationConfig, moduleMetadata} from "@storybook/angular";
+import {NzCollapseModule} from "ng-zorro-antd/collapse";
+import {NzRadioModule} from "ng-zorro-antd/radio";
 
 
 const meta: Meta<WelcomeComponent> = {
   title: 'Welcome',
-  component: WelcomeComponent
+  component: WelcomeComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [NzCollapseModule, NzRadioModule]
+    })
+  ]
 };
 
 export default meta;
